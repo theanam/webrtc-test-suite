18458d067d9d123ffad6c395d136b537f4af3400
Anam Ahmed <anam.ahmed.a@gmail.com>
2020-06-20 01:47:33 +0600

    test portal

--- test/app.js
+++ test/app.js
@@ -1,17 +1,31 @@
 import _rtc from "../index";
+import $ from "jquery";
 
+function loading(base,message){
+    base.find(".status").addClass("loading");
+    base.find(".message").html(message);
+}
+function yes(base,message){
+    base.find(".status").addClass("yes").removeClass("loading");
+    base.find(".message").html(message);
+}
+function no(base,message){
+    base.find(".status").addClass("no").removeClass("loading");
+    base.find(".message").html(message);
+}
 function init(){
-    _rtc.checkMediaCapture({audio:true,video: true},true)
-        .then(_rtc.checkPeerConnection({},true))
-        .then(result=>console.log("All tests passed"))
-        .catch(err=>console.log("err",err))
-    _rtc.countDevies(true).then(list=>console.log(list));
-    _rtc.getUserMediaSilent({video: true})
-        .then(stream=>{
-            if(!stream) return console.log("No stream could be retrieved");
-            _rtc.utils.dom.addStreamToDOM(document.querySelector(".vid"),stream).play();
-            setTimeout(()=>_rtc.utils.stream.stopMediaStreamSilent(stream),4000);
-        });
+    let pc = $(".peerconnection");
+    loading(pc,"Testing Peer Connection");
+    _rtc.checkPeerConnection({},true).then(()=>yes(pc,"Peer connection Established")).catch(()=>no(pc,"Peer connection did not work"));
+    let _net = $(".internet");
+    loading(_net,"Testing Internet Connection");
+    _rtc.checkInternetSpeed("test.png",true).then(speed=>yes(_net,`Your speed is ${speed}mbps`)).catch(()=>no(_net,"Could not test internet speed"));
+    let _cd = $(".count");
+    loading(_cd,"Counting devices");
+    _rtc.countDevies(true).then(l=>yes(_cd,`You have ${l.audio.in} audio input, ${l.audio.out} audio output, ${l.video.in} video input`)).catch(e=>no(_cd,"Could not count hardware"));
+    let um = $(".usermedia");
+    loading(um,"Testing getUserMedia");
+    _rtc.checkMediaCapture({audio:true, video: true},true).then(()=>yes(um,"Media Capture Successful")).catch(err=>no(um,"Media Capture failed"));
 }
 
 document.querySelector(".start").addEventListener("click",init);
